681122ee63a4400dabc0dc2da0d2f10af4b84c69
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-02 09:20:07 +0600

    [feat](main): add score, life and update score board
     - update lifes based on player situation
    
     - add points for right key press
    
     - upon finished life redirected to score card section
    
     - player can see their final score
    
     - can play again by pressing the button
    
     - upon pressing the esc key while playing, player quit through the game and generate the latest result
    
     - add responsiveness for all kind of devices

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -3,9 +3,20 @@ function startPlay() {
   const homeSection = document.getElementById("home-screen");
   homeSection.classList.add("hidden");
 
+  // hide final score card
+  const finalScoreCardElement = document.getElementById("final-score-card");
+  finalScoreCardElement.classList.add("hidden");
+
   // show playground screen
   const playgroundSection = document.getElementById("playground");
   playgroundSection.classList.remove("hidden");
+
+  // reset life and score
+  const lifeElement = document.getElementById("life-remaining");
+  lifeElement.innerText = 5;
+
+  const currentScoreElement = document.getElementById("current-score");
+  currentScoreElement.innerText = 0;
 
   // playing now
   playing();
@@ -32,18 +43,50 @@ document.addEventListener("keyup", handleKeyboardKeyUpEvent);
 
 function handleKeyboardKeyUpEvent(event) {
   const pressed = event.key;
+
   const currentLetter = document
     .getElementById("display-alphabet")
     .innerText.toLowerCase();
-  console.log(currentLetter);
 
   // check for match
   if (pressed === currentLetter) {
     // remove previous key highlight
     const removeKeyHighlighter = document.getElementById(currentLetter);
     removeKeyHighlighter.classList.remove("bg-[#FFA500]");
+    const currentScoreElement = document.getElementById("current-score");
+    const currentScore = currentScoreElement.innerText;
+    const newScore = parseInt(currentScore) + 1;
+    currentScoreElement.innerText = newScore;
     playing();
   } else {
-    console.log("you missed a point");
+    const lifeElement = document.getElementById("life-remaining");
+    const currentLife = lifeElement.innerText;
+    const remainingLife = parseInt(currentLife) - 1;
+    lifeElement.innerText = remainingLife;
+
+    if (remainingLife <= 0 || pressed === "Escape") {
+      // hide playground screen
+      const playgroundSection = document.getElementById("playground");
+      playgroundSection.classList.add("hidden");
+
+      // show final score card
+      const finalScoreCardElement = document.getElementById("final-score-card");
+      finalScoreCardElement.classList.remove("hidden");
+
+      // show total score
+      const totalScoreElement = document.getElementById("total-score");
+      const currentScore = document.getElementById("current-score").innerText;
+      totalScoreElement.innerText = currentScore;
+
+      // clear last selected alphabet highlight
+
+      // display alphabet
+      const currentAlphabet =
+        document.getElementById("display-alphabet").innerText;
+
+      // remove previous key highlight
+      const removeKeyHighlighter = document.getElementById(currentAlphabet);
+      removeKeyHighlighter.classList.remove("bg-[#FFA500]");
+    }
   }
 }
