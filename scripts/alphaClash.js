b8b7f97e91b9c1dc4ce1810265e812e9379da96c
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-02 06:18:56 +0600

    [feat](main): add playing function
     - create alphabet string
    
     - generate random number
    
     - generate random index
    
     - generate random alphabet using the index
    
     - display letter in the paragraph
    
     - add background color to generated alphabet key layout
    
     - remove background to clear it up for next key

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -6,4 +6,26 @@ function startPlay() {
   // show playground screen
   const playgroundSection = document.getElementById("playground");
   playgroundSection.classList.remove("hidden");
+
+  // playing now
+  playing();
+}
+
+function playing() {
+  // generate random alphabet
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+  const alphabet = alphabets[index];
+
+  // display alphabet
+  const displayLetter = document.getElementById("display-alphabet");
+  displayLetter.innerText = alphabet;
+
+  const addKeyHighlighter = document.getElementById(alphabet);
+  addKeyHighlighter.classList.add("bg-[#FFA500]");
+
+  const removeKeyHighlighter = document.getElementById(alphabet);
+  removeKeyHighlighter.classList.add("bg-[#FFA500]");
 }
